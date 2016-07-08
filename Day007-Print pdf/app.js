a797130c704a9fbb39844585b513bc1fb794dc8a
JackyTsang <ma010hkchun@gmail.com>
2016-07-08 23:34:54 +0800

    allow user to input different title, excel and image to change the content of pdf dog.jpg and tree.png are the demo image

--- Day007-Print pdf/app.js
+++ Day007-Print pdf/app.js
@@ -53,7 +53,9 @@ var storage = multer.diskStorage({
 });
 var upload = multer({storage: storage});
 var cpUpload = upload.fields([
-    {name: 'excel', maxCount: 1}
+    {name: 'title', maxCount: 1},
+    {name: 'excel', maxCount: 1},
+    {name: 'image', maxCount: 1}
 ]);
 
 app.post('/upload', function (req, res) {
@@ -62,7 +64,7 @@ app.post('/upload', function (req, res) {
             return res.end("Error uploading file." + err);
         }
         readExcel(req.files.excel);
-        makePDF('abc', req.files.excel, someText);
+        makePDF(req.body.title, req.files.excel, req.files.image, someText);
         res.sendFile(__dirname + "/views/result.html");
     });
 });
@@ -96,7 +98,7 @@ function readExcel(excel) {
 
 var someText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\nMauris at ante tellus. Vestibulum a metus lectus. Praesent tempor purus a lacus blandit eget gravida ante hendrerit. Cras et eros metus. Sed commodo malesuada eros, vitae interdum augue semper quis. Fusce id magna nunc. Curabitur sollicitudin placerat semper. Cras et mi neque, a dignissim risus. Nulla venenatis porta lacus, vel rhoncus lectus tempor vitae. Duis sagittis venenatis rutrum. Curabitur tempor massa tortor.';
 
-function makePDF(title, excel, defaultText) {
+function makePDF(title, excel, image, defaultText) {
     var doc = new pdfkit();
     doc.pipe(fs.createWriteStream('./public/file.pdf'));
 
@@ -114,6 +116,9 @@ function makePDF(title, excel, defaultText) {
         i += 20;
     }
     dynamicTextHeight = 110 + i;
+
+    doc.image('./' + image[0].path, 100, dynamicTextHeight, {fit: [100, 100]});
+    dynamicTextHeight += 120;
 
     // and some justified text wrapped into columns
     doc.text('And here is some default text...', 100, dynamicTextHeight)
