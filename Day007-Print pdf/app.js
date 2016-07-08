1a9f4f8b07affab04407600cf9b1ec78e31d2f09
JackyTsang <ma010hkchun@gmail.com>
2016-07-08 23:40:34 +0800

    add a button to download the pdf, using same method as downloading excel

--- Day007-Print pdf/app.js
+++ Day007-Print pdf/app.js
@@ -42,6 +42,11 @@ app.get('/downloadExcel', function (req, res) {
     res.download(file);
 });
 
+app.get('/downloadPdf', function (req, res) {
+    var file = './public/file.pdf';
+    res.download(file);
+});
+
 
 var storage = multer.diskStorage({
     destination: function (req, file, callback) {
