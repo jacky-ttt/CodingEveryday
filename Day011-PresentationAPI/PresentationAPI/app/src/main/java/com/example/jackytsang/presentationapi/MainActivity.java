7f57b595a5acc9f94fa65dd4aff4e6ee3130fc87
JackyTsang <ma010hkchun@gmail.com>
2016-07-15 23:57:17 +0800

    add a listener to listen add, remove and remove of a screen, https://blog.stylingandroid.com/multiple-displays-part-4/

--- Day011-PresentationAPI/PresentationAPI/app/src/main/java/com/example/jackytsang/presentationapi/MainActivity.java
+++ Day011-PresentationAPI/PresentationAPI/app/src/main/java/com/example/jackytsang/presentationapi/MainActivity.java
@@ -11,7 +11,8 @@ import java.lang.annotation.Target;
 
 public class MainActivity extends AppCompatActivity {
 
-    MyPresentation mPresentation;
+    private MyPresentation mPresentation;
+    private MyDisplayListener mDisplayListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +23,22 @@ public class MainActivity extends AppCompatActivity {
             multiInit();
     }
 
+    @Override
+    protected void onDestroy() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+            multiDestroy();
+        super.onDestroy();
+    }
+
+
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void multiInit() {
         DisplayManager dm = (DisplayManager) getSystemService(DISPLAY_SERVICE);
         if (dm == null)
             return;
 
+        mDisplayListener = new MyDisplayListener();
+        dm.registerDisplayListener(mDisplayListener, null);
         Display[] displays = dm.getDisplays(DisplayManager.DISPLAY_CATEGORY_PRESENTATION);
 
         for (Display display : displays) {
@@ -36,4 +47,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void multiDestroy() {
+        if (mDisplayListener != null) {
+            DisplayManager dm = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+            dm.unregisterDisplayListener(mDisplayListener);
+            mDisplayListener = null;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private class MyDisplayListener implements DisplayManager.DisplayListener {
+
+        @Override
+        public void onDisplayAdded(int displayId) {
+            DisplayManager dm = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+
+            Display display = dm.getDisplay(displayId);
+            if (display == null)
+                return;
+
+            mPresentation = new MyPresentation(MainActivity.this, display);
+            mPresentation.show();
+        }
+
+        @Override
+        public void onDisplayRemoved(int displayId) {
+            if (mPresentation != null && mPresentation.getDisplay().getDisplayId() == displayId) {
+                mPresentation.dismiss();
+                mPresentation = null;
+            }
+        }
+
+        @Override
+        public void onDisplayChanged(int displayId) {
+
+        }
+    }
 }
