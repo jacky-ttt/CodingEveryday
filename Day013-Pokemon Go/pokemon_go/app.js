16936b0478a38345922f68521abc76f894cf3718
JackyTsang <ma010hkchun@gmail.com>
2016-07-26 03:58:09 +0800

    pokemon go unofficial api

--- Day013-Pokemon Go/pokemon_go/app.js
+++ Day013-Pokemon Go/pokemon_go/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
+var Pokeio = require('pokemon-go-node-api');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +23,60 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+
+var a = new Pokeio.Pokeio();
+
+//Set environment variables or replace placeholder text
+var location = {
+    type: 'name',
+    name: 'Times Square'
+};
+
+var username = '<email>';
+var password = '<password>';
+var provider = 'google';
+
+a.init(username, password, location, provider, function (err) {
+    if (err) throw err;
+
+    console.log('1[i] Current location: ' + a.playerInfo.locationName);
+    console.log('1[i] lat/long/alt: : ' + a.playerInfo.latitude + ' ' + a.playerInfo.longitude + ' ' + a.playerInfo.altitude);
+
+    a.GetProfile(function (err, profile) {
+        if (err) throw err;
+
+        console.log('1[i] Username: ' + profile.username);
+        console.log('1[i] Poke Storage: ' + profile.poke_storage);
+        console.log('1[i] Item Storage: ' + profile.item_storage);
+
+        var poke = 0;
+        if (profile.currency[0].amount) {
+            poke = profile.currency[0].amount;
+        }
+
+        console.log('1[i] Pokecoin: ' + poke);
+        console.log('1[i] Stardust: ' + profile.currency[1].amount);
+
+        setInterval(function () {
+            a.Heartbeat(function (err, hb) {
+                if (err) {
+                    console.log(err);
+                }
+
+                for (var i = hb.cells.length - 1; i >= 0; i--) {
+                    if (hb.cells[i].NearbyPokemon[0]) {
+                        //console.log(a.pokemonlist[0])
+                        var pokemon = a.pokemonlist[parseInt(hb.cells[i].NearbyPokemon[0].PokedexNumber) - 1];
+                        console.log('1[+] There is a ' + pokemon.name + ' at ' + hb.cells[i].NearbyPokemon[0].DistanceMeters.toString() + ' meters');
+                    }
+                }
+
+            });
+        }, 5000);
+
+    });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
