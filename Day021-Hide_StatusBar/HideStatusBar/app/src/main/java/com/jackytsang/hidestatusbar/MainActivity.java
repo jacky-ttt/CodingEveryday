da8aedbc22170c16145e3c1de99a869d8e004b1d
JackyTsang <ma010hkchun@gmail.com>
2016-08-31 00:21:38 +0800

    request permission for drawing on top of other app
    ref:
    http://stackoverflow.com/questions/7569937/unable-to-add-window-android-view-viewrootw44da9bc0-permission-denied-for-t
    https://developer.android.com/reference/android/provider/Settings.html#canDrawOverlays(android.content.Context)

--- Day021-Hide_StatusBar/HideStatusBar/app/src/main/java/com/jackytsang/hidestatusbar/MainActivity.java
+++ Day021-Hide_StatusBar/HideStatusBar/app/src/main/java/com/jackytsang/hidestatusbar/MainActivity.java
@@ -1,13 +1,49 @@
 package com.jackytsang.hidestatusbar;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static int REQUEST_CODE = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            checkDrawOverlayPermission();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkDrawOverlayPermission() {
+        /** check if we already  have permission to draw over other apps */
+        if (!Settings.canDrawOverlays(this)) {
+            /** if not construct intent to request permission */
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            /** request permission via start activity for result */
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        /** check if received result code
+         is equal our requested code for draw permission  */
+        if (requestCode == REQUEST_CODE) {
+            /* if so check once again if we have permission */
+            /* return resultCode=0 and data=null regardless permission is granted */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+                // continue here - permission was granted
+            }
+        }
     }
 }
