c577fd9ef5b04e03d4e30922219b827da55c9ddd
JackyTsang <ma010hkchun@gmail.com>
2016-08-31 02:05:43 +0800

    hide navigation bar

--- Day021-Hide_StatusBar/HideStatusBar/app/src/main/java/com/jackytsang/hidestatusbar/MainActivity.java
+++ Day021-Hide_StatusBar/HideStatusBar/app/src/main/java/com/jackytsang/hidestatusbar/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
             public void onSystemUiVisibilityChange(int visibility) {
                 // Note that system bars will only be "visible" if none of the
                 // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
-                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                if ((visibility & (View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)) == 0) {
                     // TODO: The system bars are visible. Make any desired
                     // adjustments to your UI, such as showing the action bar or
                     // other navigational controls.
@@ -79,7 +79,11 @@ public class MainActivity extends AppCompatActivity {
     private void hideSystemBar() {
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
         decorView.setSystemUiVisibility(uiOptions);
     }
 
