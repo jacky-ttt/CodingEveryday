68713b9edd895d536d8f3664cbf2d7f0a0522c30
JackyTsang <ma010hkchun@gmail.com>
2016-09-02 00:11:12 +0800

    1. add page1 and page2 2. only refresh four selected elements

--- Day023-PJAX/routes/index.js
+++ Day023-PJAX/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/page1', function(req, res, next) {
+  res.render('page1', { title: 'Page1' });
+});
+
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Page2' });
+});
+
 module.exports = router;
