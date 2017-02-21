74c712986385d5e3e1f78c6b0fc3f46ec8cb95ce
JackyTsang <ma010hkchun@gmail.com>
2017-02-22 01:41:57 +0800

    init realm

---
+++ Day033-RealmChangeListener/RealmChangeListener/app/src/main/java/com/jackytsang/realmchangelistener/MyApplication.java
@@ -0,0 +1,16 @@
+package com.jackytsang.realmchangelistener;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+    }
+}
