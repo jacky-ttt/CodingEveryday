76bcc8cecebd5b286416e5132349cfc4c51e83d0
jacky-ttt <szechunt@gmail.com>
2018-10-09 18:31:16 +0800

    open an image in full screen using OpenCV

--- Day050-hide the top and left white line in fullscreen in OpenCV/ShowImageFullScreen/ShowImageFullScreen/ShowImageFullScreen.cpp
+++ Day050-hide the top and left white line in fullscreen in OpenCV/ShowImageFullScreen/ShowImageFullScreen/ShowImageFullScreen.cpp
@@ -1,20 +1,39 @@
-// ShowImageFullScreen.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
+using namespace cv;
+using namespace std;
 
-int main()
+
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+	string imageName("dog.jpg"); // by default
+	if (argc > 1)
+	{
+		imageName = argv[1];
+	}
+	Mat image;
+	image = imread(imageName.c_str(), IMREAD_COLOR); // Read the file
+	if (image.empty())                      // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+
+	// full screen
+	namedWindow("Display window", CV_WINDOW_NORMAL); // Create a window for display.
+	resizeWindow("Display window", 1920, 1080);
+	cvSetWindowProperty("Display window", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
+
+
+	imshow("Display window", image);
+	waitKey(0); // Wait for a keystroke in the window
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
