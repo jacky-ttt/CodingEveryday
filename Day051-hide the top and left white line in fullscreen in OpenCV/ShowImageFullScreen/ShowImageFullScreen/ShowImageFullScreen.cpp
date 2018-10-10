8f1a6c86fb19e344bf8d49a6a39de4a39ab82ed7
jacky-ttt <szechunt@gmail.com>
2018-10-10 18:49:37 +0800

    resize the image while keeping the aspect ratio (fit center)

--- Day051-hide the top and left white line in fullscreen in OpenCV/ShowImageFullScreen/ShowImageFullScreen/ShowImageFullScreen.cpp
+++ Day051-hide the top and left white line in fullscreen in OpenCV/ShowImageFullScreen/ShowImageFullScreen/ShowImageFullScreen.cpp
@@ -7,7 +7,28 @@
 using namespace cv;
 using namespace std;
 
+cv::Mat ResizeKeepAspectRatio(const cv::Mat &input, const cv::Size &dstSize, const cv::Scalar &bgcolor)
+{
+	cv::Mat output;
 
+	double h1 = dstSize.width * (input.rows / (double)input.cols);
+	double w2 = dstSize.height * (input.cols / (double)input.rows);
+	if (h1 <= dstSize.height) {
+		cv::resize(input, output, cv::Size(dstSize.width, h1));
+	}
+	else {
+		cv::resize(input, output, cv::Size(w2, dstSize.height));
+	}
+
+	int top = (dstSize.height - output.rows) / 2;
+	int down = (dstSize.height - output.rows + 1) / 2;
+	int left = (dstSize.width - output.cols) / 2;
+	int right = (dstSize.width - output.cols + 1) / 2;
+
+	cv::copyMakeBorder(output, output, top, down, left, right, cv::BORDER_CONSTANT, bgcolor);
+
+	return output;
+}
 
 int main(int argc, char** argv)
 {
@@ -29,8 +50,8 @@ int main(int argc, char** argv)
 	namedWindow("Display window", CV_WINDOW_NORMAL); // Create a window for display.
 	cvSetWindowProperty("Display window", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
 
-
-	imshow("Display window", image);
+	Mat resizedImage = ResizeKeepAspectRatio(image, Size(1920, 1080), Scalar(0, 0, 0));
+	imshow("Display window", resizedImage);
 	waitKey(0); // Wait for a keystroke in the window
 
 
