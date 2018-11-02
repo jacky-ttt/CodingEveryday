a8fe8223861b89ada8a6904cc8b1fc023e053aab
jacky-ttt <szechunt@gmail.com>
2018-11-02 19:47:12 +0800

    start another activity  when click the button in the main activity

---
+++ Day054-Organize res folder better/OrganizeRes/app/src/main/java/com/example/jackytsang/organizeres/anotherActivity/AnotherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jackytsang.organizeres.anotherActivity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.jackytsang.organizeres.R
+
+class AnotherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_another)
+    }
+}
